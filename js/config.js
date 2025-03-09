06cbd39114b2738bb8c52a074005060a391850a9
Kerolos Ibrahim <kerolosibrahimbusi@gmail.com>
2025-03-09 22:05:41 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_NPcaWwW7U', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '10bek8n72pes3e66thgikhun15', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5974ni3st2nipmdb7ccqr5lk05', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
